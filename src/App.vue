1ae6f20bc73e4cf1c21e9b742eaa3954a2a6ecc4
parkkyungho <dggb93@smartscore.kr>
2023-07-05 17:53:19 +0900

    vue cli 넣기

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="particles-js">
+    <div class="header">
+      <h1>
+        <span class="site-title">ADITYA VIKRAM SINGH</span>
+        <span class="site-description">Programmer • Web Developer • Graphic Designer</span>
+      </h1>
+      <div class="header-icons">
+        <a aria-label="My LinkedIn Profile" target="_blank" href="https://www.linkedin.com/in/avs1508/">
+          <i class="icon fa fa-linkedin" aria-hidden="true"></i>
+        </a>
+        <a aria-label="My Github Profile" target="_blank" href="https://github.com/AVS1508">
+          <i class="icon fa fa-github-alt" aria-hidden="true"></i>
+        </a>
+        <a aria-label="My Résumé" target="_blank" href="https://www.adityavsingh.com/resume.html">
+          <i class="icon fa fa fa-file-pdf-o" aria-hidden="true"></i>
+        </a>
+        <a aria-label="Send Email" href="mailto:avsingh@umass.edu" target="_blank"
+          ><i class="icon fa fa-envelope"></i
+        ></a>
+      </div>
+      <div class="header-links">
+        <a class="link" href="#about" data-scroll>About Me</a>
+        <a class="link" href="#projects" data-scroll>Projects</a>
+      </div>
+    </div>
+    <a class="down" href="#about" data-scroll><i class="icon fa fa-chevron-down" aria-hidden="true"></i></a>
+  </div>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style>
+
+</style>
