57fdee220f9898531fd3882c708b994db18a1223
dggb <dggb2@naver.com>
2023-07-31 20:46:36 +0900

    커리어 작업

--- src/components/career-component.vue
+++ src/components/career-component.vue
@@ -15,7 +15,7 @@
 
           <div class="careerBody">
             <div class="careerDis">
-              <div style="padding-right: 3rem;border-right: 1px solid #ccc;">
+              <div class="careerLeft">
                 <div style="" class="careerLogo">
                     <img src="../../public/assets/img/png/simplebit.png" style="display: block;flex-shrink: 0;width: 90%;" />
                 </div>
@@ -24,6 +24,40 @@
               <div class="careerText">
                   <div class="careerTitle">(주) 심플비트</div>
                   <div>재직 기간 : 2018.10 ~ 2022.08 (3년 10개월)</div>
+                  <div>직급 및 직책  : 대리 / 웹 개발자</div>
+                  <br>
+                  <div>
+                    (주)심플비트는 <b>SHMS(Structural Health Monitoring System) 솔루션을 기반으로 고객의 요구에
+                    대응하는 소프트웨어를</b> 개발하는 회사입니다.
+                    <br>
+                    <br>
+                    ▎담당 업무 
+                    <br>
+                     - 웹 개발에 필요 한 서버 구축 및 운용
+                    <br>
+                     - 클라이언트 협업(요구사항 및 디자인)
+                    <br>
+                     - 웹 개발 (프론트 엔트-Vue.js, 백 엔드-Node.js, SQL-MySQL,PostgreSQL, 소규모 웹 개발-Visual Studio C#)
+                    <br>
+                     - 앱 개발 (Flutter로 로그인 만 구현 후 웹뷰 및 Vue.js로 개발)              
+                  </div>
+                  <hr style="border-top:1px solid #ccc">
+              </div>
+            </div>
+          </div>
+
+          <div class="careerBody">
+            <div class="careerDis">
+              <div class="careerLeft">
+                <div class="careerLogo">
+                    <img src="../../public/assets/img/png/smartscore.png" style="display: block;flex-shrink: 0;width: 90%;" />
+                </div>
+              </div>
+
+              <div class="careerText">
+                  <div class="careerTitle">(주) 스마트스코어</div>
+                  <div>재직 기간 : 2023.04 ~ 2023.08 (4개월)</div>
+                  <div>직급 및 직책 : 매니저 / 솔루션 ERP팀 FrontEnd 개발자</div>
                   <br>
                   <div>
                     (주)심플비트는 <b>SHMS(Structural Health Monitoring System) 솔루션을 기반으로 고객의 요구에
